3ef3ddd9f442fb13a96b843dd5668837e56c96d8
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-04 21:54:09 +0530

    product list and pages setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import NavBar from '../features/navbar/Navbar';
+import ProductList from '../features/product-list/ProductList';
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar>
+        <ProductList />
+      </NavBar>
+    </div>
+  );
+}
