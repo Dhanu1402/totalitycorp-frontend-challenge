017d76e255ae0f07e565d191e72190ef84ab3a76
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-06 22:08:53 +0530

    brands and category api

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -15,13 +15,11 @@ export function fetchProductsByFilters(filter, sort, pagination) {
   // pagination = {_page:1,_limit=10}
   // TODO : on server we will support multi values in filter
   let queryString = '';
-
   for (let key in filter) {
     const categoryValues = filter[key];
 
     if (categoryValues.length) {
       const lastCategoryValue = categoryValues[categoryValues.length - 1];
-
       queryString += `${key}=${lastCategoryValue}&`;
     }
   }
@@ -46,5 +44,25 @@ export function fetchProductsByFilters(filter, sort, pagination) {
     const totalItems = await response.headers.get('X-Total-Count');
 
     resolve({ data: { products: data, totalItems: +totalItems } });
+  });
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch('http://localhost:8080/categories');
+
+    const data = await response.json();
+
+    resolve({ data });
+  });
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch('http://localhost:8080/brands');
+
+    const data = await response.json();
+
+    resolve({ data });
   });
 }
