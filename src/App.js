b04d79834f456d7eb7a6eab823fa1d81d1e74bb9
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-07 11:22:53 +0530

    order status

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import { Counter } from './features/counter/Counter';
 import './App.css';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
-import ProductDetailPage from './pages/ProductDetailPage';
 import SignupPage from './pages/SignupPage';
+
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
 } from 'react-router-dom';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
 import Protected from './features/auth/components/Protected';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectLoggedInUser } from './features/auth/authSlice';
 import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
-
+import PageNotFound from './pages/404';
+import OrderSuccessPage from './pages/OrderSuccessPage';
+import UserOrders from './features/user/components/UserOrders';
+import UserOrdersPage from './pages/UserOrdersPage';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -28,11 +34,11 @@ const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <LoginPage />,
+    element: <LoginPage></LoginPage>,
   },
   {
     path: '/signup',
-    element: <SignupPage />,
+    element: <SignupPage></SignupPage>,
   },
   {
     path: '/cart',
@@ -58,11 +64,25 @@ const router = createBrowserRouter([
       </Protected>
     ),
   },
+  {
+    path: '/order-success/:id',
+    element: <OrderSuccessPage></OrderSuccessPage>,
+  },
+  {
+    path: '/orders',
+    element: (
+      <UserOrdersPage></UserOrdersPage>
+      // we will add Page later right now using component directly.
+    ),
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>,
+  },
 ]);
 
 function App() {
   const dispatch = useDispatch();
-
   const user = useSelector(selectLoggedInUser);
 
   useEffect(() => {
--- src/features/cart/cartAPI.js
+++ src/features/cart/cartAPI.js
@@ -5,7 +5,6 @@ export function addToCart(item) {
       body: JSON.stringify(item),
       headers: { 'content-type': 'application/json' },
     });
-
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
     resolve({ data });
@@ -16,9 +15,7 @@ export function fetchItemsByUserId(userId) {
   return new Promise(async (resolve) => {
     //TODO: we will not hard-code server URL here
     const response = await fetch('http://localhost:8080/cart?user=' + userId);
-
     const data = await response.json();
-
     resolve({ data });
   });
 }
@@ -30,7 +27,6 @@ export function updateCart(update) {
       body: JSON.stringify(update),
       headers: { 'content-type': 'application/json' },
     });
-
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
     resolve({ data });
@@ -43,9 +39,20 @@ export function deleteItemFromCart(itemId) {
       method: 'DELETE',
       headers: { 'content-type': 'application/json' },
     });
-
     const data = await response.json();
     // TODO: on server it will only return some info of user (not password)
     resolve({ data: { id: itemId } });
+  });
+}
+
+export function resetCart(userId) {
+  // get all items of user's cart - and then delete each
+  return new Promise(async (resolve) => {
+    const response = await fetchItemsByUserId(userId);
+    const items = response.data;
+    for (let item of items) {
+      await deleteItemFromCart(item.id);
+    }
+    resolve({ status: 'success' });
   });
 }
