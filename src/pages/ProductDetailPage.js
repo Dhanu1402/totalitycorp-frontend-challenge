4de92fcd67802f781de36f26683a24e794977ef3
dhanu1402 <dhanukumar2000@gmail.com>
2023-09-07 15:38:35 +0530

    final ui changes with alerts, logo footers loaders and popups

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,10 +1,16 @@
 import NavBar from '../features/navbar/Navbar';
 import ProductDetail from '../features/product/components/ProductDetail';
+import Footer from '../features/common/Footer';
 
-export default function ProductDetailPage() {
+function ProductDetailPage() {
   return (
-    <NavBar>
-      <ProductDetail />
-    </NavBar>
+    <div>
+      <NavBar>
+        <ProductDetail></ProductDetail>
+      </NavBar>
+      <Footer></Footer>
+    </div>
   );
 }
+
+export default ProductDetailPage;
